98658bbb9842ac1be8de851806cbfcb814f067e3
Hoang Nhan <157661997+HoangNhan11224865@users.noreply.github.com>
2024-03-14 08:38:57 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3=new MP3("song","a0",18,11);
+        MP3 mp3=new MP3("song","a0",1B,11);
         mp3.showInfo();
 
-        Book book=new Book("name","a1",2,"author",23,"not");
+        Book book=new Book("name","a1",2,"author",200K,"not");
         book.showInfo();
     }
 }
